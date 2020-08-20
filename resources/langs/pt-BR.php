919dec26ed83296e65d6ed8bbd2bceb85b791745
Alan Benazzi <alan.benazzi@gmail.com>
2020-08-20 20:33:04 -0300

    Added Brazilian Portuguese translation

---
+++ resources/langs/pt-BR.php
@@ -0,0 +1,46 @@
+<?php
+
+//Note: Commented keys already exist in webtrees translations.
+
+use Fisharebest\Webtrees\I18N;
+
+return [
+    //Frontend: Modals
+    'Enter individuals id or something else' => 'Informar ID da pessoa ou algo mais',
+    'This operation can not be undone' => 'Esta operação não pode ser desfeita',
+    'You have enabled "Create links" feature' => 'Você habilitou a funcionalidade "Criar ligações"',
+    'Linked individuals will not be detached from media' => 'Pessoas relacionadas não serão desanexadas da mídia',
+    'Are you sure that want delete %s from image?' => 'Você tem certeza que quer excluir %s da imagem?',
+    //Config: JavaScript
+    'Are you sure?' => 'Você tem certeza?',
+    'Read more' => 'Leia mais',
+    //Config
+    'Reset filters' => 'Redefinir filtros',
+    //Config: Missed
+    'Missed' => 'Faltante',
+    //'Remove' => 'Remove',
+    'Remove all records without media' => 'Remover todos os registros sem mídia',
+    'Find' => 'Encontrar',
+    'Try to find missed records by filename' => 'Tentar encontrar registros faltantes por nome de arquivo',
+    //Config: Settings
+    'Settings' => 'Configurações',
+    'Read XMP data' => 'Ler dados XMP',
+    'Read and show XMP data (such as Goggle Picasa face tags) from media file' => 'Ler e exibir dados XMP (como tags de rosto Goggle Picasa) do arquivo de mídia',
+    'Create links' => 'Criar ligações',
+    'Link individual with media when mark them on photo' => 'Relacionar pessoa com a mídia quando marcá-la na foto',
+    'Show meta' => 'Mostrar meta',
+    'Load and show information from linked fact' => 'Carregar e exibir informação do fato relacionado',
+    'Show tab' => 'Exibir aba',
+    'Show tab on individuals page' => 'Exbir aba na página pessoal',
+    //Config: Table
+    //'Media' => 'Mídia',
+    //'Notes' => 'Notas',
+    //'Status' => 'Status',
+    'Actions' => 'Ações',
+    //Config: Messages
+    '%s record' . I18N::PLURAL . '%s records' => '%s registro' . I18N::PLURAL . '%s registros',
+    'has been deleted' . I18N::PLURAL . 'have been deleted' => 'foi excluído' . I18N::PLURAL . 'foram excluídos',
+    'has been repaired' . I18N::PLURAL . 'have been repaired' => 'foi reparado' . I18N::PLURAL . 'foram reparados',
+    //'Enabled' => 'Habilitado',
+    'Disabled' => 'Desabilitado',
+];
